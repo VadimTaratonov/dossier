a07cc6be9b946f642ec065943ad1c532b4f62e2e
Vadim Taratonov <taratonovv8@gmail.com>
2023-08-26 20:47:11 +0300

    Add exception

---
+++ src/main/java/ru/taratonov/dossier/exception/IllegalDataFromOtherMsException.java
@@ -0,0 +1,7 @@
+package ru.taratonov.dossier.exception;
+
+public class IllegalDataFromOtherMsException extends RuntimeException {
+    public IllegalDataFromOtherMsException(String message) {
+        super(message);
+    }
+}
