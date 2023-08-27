dd378df17d4387aa2d3a0d2e6ba46cc42e6b6c3f
Vadim Taratonov <taratonovv8@gmail.com>
2023-08-27 23:03:48 +0300

    Update dto and application.properties

--- src/main/java/ru/taratonov/dossier/dto/EmailMessageDTO.java
+++ src/main/java/ru/taratonov/dossier/dto/EmailMessageDTO.java
@@ -1,5 +1,6 @@
 package ru.taratonov.dossier.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,21 @@ import ru.taratonov.dossier.enums.Theme;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailMessageDTO {
+    @Schema(
+            description = "client's email",
+            name = "emailAddress",
+            example = "taratonovv8@bk.ru")
     private String emailAddress;
+
+    @Schema(
+            description = "Theme of email message",
+            name = "theme",
+            example = "CREATE_DOCUMENTS")
     private Theme theme;
+
+    @Schema(
+            description = "application id",
+            name = "applicationId",
+            example = "1")
     private Long applicationId;
 }
